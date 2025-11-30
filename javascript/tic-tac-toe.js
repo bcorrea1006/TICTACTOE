f05deabd010b34af4650c08088e3a45b7622dce9
bcorrea1006 <benitoscorrea@gmail.com>
2025-11-29 16:58:12 -0800

    added todos

--- javascript/tic-tac-toe.js
+++ javascript/tic-tac-toe.js
@@ -1,3 +1,5 @@
+const readline = require('readline');
+
 let board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
@@ -9,10 +11,10 @@ function main() {
   for (let turn = 0; turn < 9; turn++) {
     console.log("Turn " + (turn + 1))
     displayBoard();
-    // Get user input (row, col)
+    // TODO: Get user input (row, col)
     let [row, col] = getUserInput();
-    // Check the winner
-    // Update board and toggle player
+    // TODO: Check the winner
+    // TODO: Update board and toggle player
     board[row][col] = current_player;
     current_player = current_player == 'X' ? 'O' : 'X';
   }
@@ -29,8 +31,18 @@ function displayBoard() {
   console.log();
 }
 
+// TODO: handle invalid input, get the input etc.
 function getUserInput() {
-  return [2, 1];
+  // Create an interface for input and output
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question('What is your name? ', (answer) => {
+    console.log(`Hello, ${answer}!`);
+    rl.close(); // Close the readline interface after getting the answer
+  });
 }
 
 // Call main to start the program
